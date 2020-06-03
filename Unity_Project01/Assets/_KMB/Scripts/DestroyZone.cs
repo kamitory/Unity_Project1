6650d95cf7007fcb47a05f9504d845762c43d907
kamitory <necohoosen@gmail.com>
2020-06-03 12:27:06 +0900

    디스트로이존 생성
    - 큐브를 이용한 디스트로이존 생성
    - 충돌레이어 설정

---
+++ Unity_Project01/Assets/_KMB/Scripts/DestroyZone.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyZone : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        //이곳에서 트리거에 감지됨 오브젝트 제거하기 (총알, 에너미)
+        Destroy(other.gameObject);
+    }
+}
