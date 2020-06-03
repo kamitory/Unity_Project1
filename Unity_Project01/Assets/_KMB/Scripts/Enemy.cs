f1bfe0c537fd4aba27abe5f353dffca40d8dad57
kamitory <necohoosen@gmail.com>
2020-06-03 11:38:35 +0900

    에너미 클래스 및 에너미매니져 생성
    - 에너미 프리팹
    - 충돌처리 (에너미와 플레이어, 플레이어 총알)
    - 에너미 매니져를 통한 자동생성

---
+++ Unity_Project01/Assets/_KMB/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float speed = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //아래로이동
+        transform.Translate(Vector3.down*speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //자신도삭제 충동대상도삭제
+        //Destroy(gameObject,1.0f);     //1초뒤에 삭제
+        Destroy(gameObject);
+        Destroy(collision.gameObject); //충돌한대상이 collision이다
+    }
+}
---
+++ Unity_Project01/Assets/_KMB/Scripts/EnemyManager.cs
@@ -0,0 +1,46 @@
+﻿//using System; //이게있으면 랜덤함수 사용불가
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    //에너미매니저의 역할
+    //에너미 프리팹을 찍어낸다
+    //에너미 스폰타임
+    //에너미 스폰위치
+
+    public GameObject enemyFactory;     //프리팹을받아옴
+    public GameObject spawnPoint;       //스폰될위치
+    public GameObject[] spawnPoints;       //스폰될위치
+    float spawnTime = 1.0f;                    //스폰쿨타임
+    float curTime = 0.0f;                      //스폰누적타임
+    
+    // Update is called once per frame
+    void Update()
+    {
+        //에너미 생성
+        spawnEnemy();
+    }
+
+    private void spawnEnemy()
+    {
+        //몇초에 한번씩 이벤트 발동
+        //시간누적타임으로 계산
+        //게임에서 자주이용
+        curTime += Time.deltaTime;
+        if(curTime > spawnTime)
+        {
+            //누적된 시간을 초기화
+            curTime = 0.0f;
+            spawnTime = Random.Range(0.5f, 2.0f);
+            //에너미 생성
+            GameObject enemy = Instantiate(enemyFactory);
+            enemy.transform.position = spawnPoint.transform.position;
+            //enemy.transform.position = spawnPoint.transform.position;
+            int index = Random.Range(0, spawnPoints.Length);
+            //enemy.transform.position = transform.GetChild(index).position;
+            enemy.transform.position = spawnPoints[index].transform.position;
+        }
+    }
+}
